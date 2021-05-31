e7a54003e4cb1b1e5f7cb0d50dc9d79a09de6085
cosmin-ferent <cosmin_ferent@yahoo.com>
2021-05-31 06:55:06 +0300

    Hacker Rank exercises
    first exercises

---
+++ fizz_buzz.py
@@ -0,0 +1,13 @@
+def fiz_buzz(number):
+    for i in range(number):
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+        elif i % 3 == 0:
+            print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        else:
+            print(i)
+
+
+fiz_buzz(int(input('Enter your number: ')))
--- list_fibonacci.py
+++
@@ -1,7 +0,0 @@
-fib = [0, 1]
-
-for _ in range(int(input('Enter your range number: '))):
-    new_item = fib[-1] + fib[-2]
-    fib.append(new_item)
-
-print(fib)
